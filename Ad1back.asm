418d7a6a8a3215227f313f66f76eb37bd19bd19e
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-08-27 03:22:28 +0530

    OS

--- Ad1back.asm
+++ Ad1back.asm
@@ -3,14 +3,21 @@ SECTION .TEXT
     GLOBAL Addition
 
 Addition:
+    ;mov eax, edi
+    ;add eax, esi
+    ;ret
+
     mov rax, rdi
     add rax, rsi
+    ;mov ecx, eax
 
+    ;mov ebx,1
+    ;mov eax,4
+    ;int 0x80
     mov rax, 1
     mov rdi, 1
-    
-    ;mov rsi, message
-  	;mov rdx, 13
+    mov rsi, message
+  	mov rdx, 13
     syscall
 
     ;system exit
--- add.c
+++ add.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 
-extern int Addition(int a , int b);
+extern void Addition(int a , int b);
 void main(){
 	int num1, num2;
 	printf("Enter num1 : ");
 	scanf("%d",&num1);
-	printf("\nEnter num2 : ");
+	printf("Enter num2 : ");
 	scanf("%d",&num2);
-	int sum =Addition(num1,num2);
-	printf("\n %d", sum);
-
-
+	printf("\n The Sum is: ");
+	Addition(num1,num2);
 }
