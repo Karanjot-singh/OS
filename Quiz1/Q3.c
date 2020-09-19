2bd2c1dfb4d82350521d2cf37408f422e2e48ccd
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-09-19 09:37:56 +0530

    Quiz Files edited

--- Quiz1/Q3.c
+++ Quiz1/Q3.c
@@ -1,32 +1,25 @@
 #include<stdio.h>
 
 int main(int argc, char *argv[]){
+	int n;
+	long int l;// Any random 
+	char x[64];
 
-	int noOfInts = 16;
-	int noOfLongInts = 8;
 
-	int *p1 = (long int*)malloc(n * sizeof(long int)); 
-	int *p2;
-	 if (ptr == NULL) { 
-        printf("Memory not allocated.\n"); 
-        exit(0); 
-    } 
-    else { 
-  
-        // Memory has been successfully allocated 
-        printf("Memory successfully allocated using malloc.\n"); 
-  
-        // Get the elements of the array 
-        for (i = 0; i < n; ++i) { 
-            ptr[i] = i + 1; 
-        } 
-  
-        // Print the elements of the array 
-        printf("The elements of the array are: "); 
-        for (i = 0; i < n; ++i) { 
-            printf("%d, ", ptr[i]); 
-        } 
-    } 
+    //64 bit ints
+	for (int i = 0; i < 8; ++i){
+	    long int *p1 = (long int*)(&x[0]);
+		*(p1+i) = l;
+	}
+	
+	//32 bit ints
+	for (int i = 0; i < 16; ++i){
+	    int *p2 = (int*)(&x[0]);
+		*(p2+i) = n;
+	}
+
+	//malloc code
+
 
 	return 0;
 }
