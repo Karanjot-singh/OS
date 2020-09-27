631c35f98c31227e50868dbb3e822c940e0540dd
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-09-27 15:11:58 +0530

    New Files added

--- Assignment 1/f.c
+++ Assignment 1/f.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include<stdlib.h> 
 #include<sys/wait.h> 
@@ -11,12 +10,54 @@
 
 
 void display(){
-	
+	printf("---Terminal---\n Available commands:\n‘cd’, ‘echo’, ‘history’, ‘pwd’ and ‘exit’\n‘ls’, ‘cat’, ‘date’, ‘rm’ and ‘mkdir’\n" );
 
 }
 int main(){
-	display();
+	display();	
+	printf("Enter command number: \n");
+	int choice,loopf =1;
+	while(loopf){ 
+		scanf("%d",&choice);
+		if(choice>=1 && choice <=10){
+			printf("Command selected!\n");
+		}
+		else
+			printf("Enter valid command number!!\n");
+	}
+	switch(choice){
+		case 1:{
 
+		}
+		case 2:{
+			
+		}
+		case 3:{
+			
+		}
+		case 4:{
+			
+		}
+		case 5:{
+			
+		}
+		case 6:{
+			
+		}
+		case 7:{
+			
+		}
+		case 8:{
+			
+		}
+		case 9:{
+			
+		}
+		case 10:{
+			
+		}
+
+	}
 
 
 
