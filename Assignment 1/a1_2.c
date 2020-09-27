0938780a16e91d3ee44a26822a651775a078ec80
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-09-27 15:40:08 +0530

    New Files added

--- Assignment 1/a1_2.c
+++ Assignment 1/a1_2.c
@@ -1,1 +1,66 @@
-a1_2.c
+#include <stdio.h>
+#include <unistd.h>
+#include<stdlib.h> 
+#include<sys/wait.h> 
+#include <sys/types.h>
+
+// Your task is to design your a simple shell that can handle five, internal commands –
+ // ‘cd’, ‘echo’, ‘history’, ‘pwd’ and ‘exit’. These commands
+// external commands – ‘ls’, ‘cat’, ‘date’, ‘rm’ and ‘mkdir’.
+
+
+void display(){
+	printf("---Terminal---\n Available commands:\n‘cd’, ‘echo’, ‘history’, ‘pwd’ and ‘exit’\n‘ls’, ‘cat’, ‘date’, ‘rm’ and ‘mkdir’\n" );
+
+}
+int main(){
+	display();	
+	printf("Enter command number: \n");
+	int choice,loopf =1;
+	while(loopf){ 
+		scanf("%d",&choice);
+		if(choice>=1 && choice <=10){
+			printf("Command selected!\n");
+		}
+		else
+			printf("Enter valid command number!!\n");
+	}
+	switch(choice){
+		case 1:{
+
+		}
+		case 2:{
+			
+		}
+		case 3:{
+			
+		}
+		case 4:{
+			
+		}
+		case 5:{
+			
+		}
+		case 6:{
+			
+		}
+		case 7:{
+			
+		}
+		case 8:{
+			
+		}
+		case 9:{
+			
+		}
+		case 10:{
+			
+		}
+
+	}
+
+
+
+
+	return 0;
+}
--- Assignment 1/f.c
+++ Assignment 1/f.c
@@ -1,66 +1,81 @@
 #include <stdio.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include<stdlib.h> 
 #include<sys/wait.h> 
 #include <sys/types.h>
-
-// Your task is to design your a simple shell that can handle five, internal commands –
- // ‘cd’, ‘echo’, ‘history’, ‘pwd’ and ‘exit’. These commands
-// external commands – ‘ls’, ‘cat’, ‘date’, ‘rm’ and ‘mkdir’.
-
-
-void display(){
-	printf("---Terminal---\n Available commands:\n‘cd’, ‘echo’, ‘history’, ‘pwd’ and ‘exit’\n‘ls’, ‘cat’, ‘date’, ‘rm’ and ‘mkdir’\n" );
-
-}
 int main(){
-	display();	
-	printf("Enter command number: \n");
-	int choice,loopf =1;
-	while(loopf){ 
-		scanf("%d",&choice);
-		if(choice>=1 && choice <=10){
-			printf("Command selected!\n");
-		}
-		else
-			printf("Enter valid command number!!\n");
-	}
-	switch(choice){
-		case 1:{
+	pid_t pid;
+	int status;
+	pid = fork();
 
-		}
-		case 2:{
-			
-		}
-		case 3:{
-			
-		}
-		case 4:{
-			
-		}
-		case 5:{
-			
-		}
-		case 6:{
-			
-		}
-		case 7:{
-			
-		}
-		case 8:{
-			
-		}
-		case 9:{
-			
-		}
-		case 10:{
-			
-		}
-
+	if(pid<0){
+		printf("Error in fork process");
+	return 1;
 	}
 
+	else if(pid ==0){
+	//child process
+		char content[400];
+		int fd = open("", O_RDONLY );
+		if ( fd < 0 )
+        return 1;
+
+	    off_t fileLength = lseek( fd, 0, SEEK_END );  // goes to end of file
+	    if ( fileLength < 0 )
+	        return 1;
+	    ssize_t bytes = read (fd,content,sizeof(content)-1);
+	    content[400]='\0';
+	    write(1,content,bytes);
+	    close(fd);
+		exit(0);
+	}
+
+	else{
+	// parent
+		pid_t parent_id = waitpid(pid, &status, 0);
 
 
-
+	}
 	return 0;
+
 }
+
+
+
+// #include<stdio.h> 
+// #include<conio.h> 
+// #include<String.h> 
+// void getData(char *buff); 
+// int main() 
+// { 
+// 	FILE *fp = fopen("D:\\Test.csv","r"); 
+//     int count=0; 
+// 	do 
+// 	{ 
+// 	 char buff[1024]; 
+// 	 fgets(buff, 1024, (FILE*)fp); 
+// 	 count++; 
+// 	 if(count != 1) 
+// 	 { 
+// 	  printf(buff); 
+// 	  getData(buff); 
+// 	 } 
+// 	 getch(); 
+// 	}while((getc(fp))!=EOF); 
+ 
+	 
+// } 
+// void getData(char buff[]) 
+// { 
+//    char *token = strtok(buff,","); 
+//    int counter=0; 
+ 
+//    while( token != NULL )  
+//    { 
+//  counter++;  
+// printf( " %s\n",token); 
+//       token = strtok(NULL,","); 
+//    }	   
+// } 
+
