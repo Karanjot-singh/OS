d261c6175d728753aba91eeb17c7a86f265f358d
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-09-28 23:50:24 +0530

    Assignment Q1

--- Assignment 1/new1.c
+++ Assignment 1/new1.c
@@ -60,65 +60,17 @@ int main(){
 
 	else if(pid ==0){
 	//child process
-		char content[7000];
-		int fd = open( "file.csv", O_RDONLY );
-
-		if (fd < 0){
-			return 1;
-			printf("Error\n");
-		}
-
-		read (fd,content,sizeof(content)-1);
-		/*Process Data for getData()*/
-		content[7000]='\0';
-		int count =0;
-		int prev_index=0;
-		int char_count=0;
-		char ch ='\0';
-		for(int i =0 ; i<7000;i++){
-	    	// printf("%c",content[i] );
-
-			if (content[i]=='\n')
-			{	
-				count++;
-				char buff[100];
-				strncat(buff,&content[prev_index],char_count);
-				strncat(buff,&ch,1);			
-				if (count>1)
-				{	
-					// printf("%s\n",buff);
-					getData(buff);
-				}
-				prev_index+=char_count+1;
-				char_count=0;
-				strcpy(buff,"");
-
-			}
-			else if(content[i]=='B'){
-				//For reading only sec A marks
-				break;
-			}
-			else
-				char_count++;
-		}
-
-		close(fd);
-		exit(0);
-	}
-
-	else{
-	// parent
-		// pid_t parent_id = waitpid(pid, &status, 0);
-		// 		char content[7000];
+		printf("Child Process:\n");
+		// char content[7000];
 		// int fd = open( "file.csv", O_RDONLY );
 
 		// if (fd < 0){
-		// 	return 1;
-		// 	printf("Error\n");
+		// 	perror("Error ");
+		// 	return -1;
 		// }
 
 		// read (fd,content,sizeof(content)-1);
-
+		// /*Process Data for getData()*/
 		// content[7000]='\0';
 		// int count =0;
 		// int prev_index=0;
@@ -129,33 +81,22 @@ int main(){
 
 		// 	if (content[i]=='\n')
 		// 	{	
-	 //    		// printf("p%d c%d\n",prev_index,char_count );
 		// 		count++;
 		// 		char buff[100];
 		// 		strncat(buff,&content[prev_index],char_count);
 		// 		strncat(buff,&ch,1);			
 		// 		if (count>1)
 		// 		{	
-		// 			printf("%s\n",buff);
-		// 			// getData(buff);
+		// 			// printf("%s\n",buff);
+		// 			getData(buff);
 		// 		}
 		// 		prev_index+=char_count+1;
 		// 		char_count=0;
 		// 		strcpy(buff,"");
 
 		// 	}
-		// else if(content[i]=='A' && count>1){
-		// 		//For reading only sec B marks
-		// 		break;
-		// 	}
-		// 	else if(content[i]=='@'){
-		// 		char temp[100];
-		// 		strncat(temp,&content[prev_index],char_count);
-		// 		strncat(temp,&ch,1);
-		// 		printf("%s\n",temp);
-		// 		// getData(temp);
-		// 		prev_index+=char_count+1;
-		// 		char_count=0;
+		// 	else if(content[i]=='B'){
+		// 		//For reading only sec A marks
 		// 		break;
 		// 	}
 		// 	else
@@ -163,6 +104,69 @@ int main(){
 		// }
 
 		// close(fd);
+		// exit(0);
+	}
+
+	else{
+	// parent
+		printf("Parent Process:\n");
+
+		pid_t parent_id = waitpid(pid, &status, 0);
+		char content[7000];
+		int fd = open( "file.csv", O_RDONLY );
+
+		if (fd < 0){
+			return 1;
+			printf("Error\n");
+		}
+
+		read (fd,content,sizeof(content)-1);
+
+		content[7000]='\0';
+		int count =0;
+		int prev_index=0;
+		int char_count=0;
+		char ch ='\0';
+		for(int i =0 ; i<7000;i++){
+	    	// printf("%c",content[i] );
+
+			if (content[i]=='\n')
+			{	
+	    		// printf("p%d c%d\n",prev_index,char_count );
+				count++;
+				char buff[100];
+				strncat(buff,&content[prev_index],char_count);
+				strncat(buff,&ch,1);			
+				if (count>1)
+				{	
+					// printf("%s\n",buff);
+					// getData(buff);
+				}
+				printf("%s\n",buff);
+				prev_index+=char_count+1;
+				char_count=0;
+				strcpy(buff,"");
+
+			}
+			else if(content[i]=='A' && count>1){
+				//For reading only sec B marks
+				continue;
+			}
+			else if(content[i]=='@'){
+				char temp[100];
+				strncat(temp,&content[prev_index],char_count);
+				strncat(temp,&ch,1);
+				printf("%s\n",temp);
+				// getData(temp);
+				prev_index+=char_count+1;
+				char_count=0;
+				break;
+			}
+			else
+				char_count++;
+		}
+
+		close(fd);
 
 	}
 	return 0;
