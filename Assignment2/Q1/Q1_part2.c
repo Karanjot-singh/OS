897f85b94b7c144c813e11bc7dc05b72ea8230a4
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-10-28 23:04:30 +0530

    Files added for OS Assignment 2

--- Assignment2/Q1/Q1_part2.c
+++ Assignment2/Q1/Q1_part2.c
@@ -1,51 +1,35 @@
 /* Name: Karanjot Singh
    Roll_Number: 2019050 */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
+
 int counter = 10;
 
-void *parent_thread()
+void parent_thread()
 {
 	while (counter != 100)
-	{
-		++counter;
-		printf("Parent thread\n");
-		printf("%d ", counter);
-	}
-	printf("\n");
+		printf("%d Parent thread\n", ++counter);
 }
 void *child_thread()
 {
-
 	while (counter != -90)
-	{
-		--counter;
-		printf("Child thread\n");
-		printf("%d ", counter);
-	}
-	printf("\n");
+		printf("%d Child Thread\n", --counter);
 }
 
 int main()
 {
-	pthread_t parent;
 	pthread_t children;
-
-	int thread1 = pthread_create(&parent, NULL, parent_thread, NULL);
-	if (thread1)
-	{
-		perror("Error in creating thread");
-		exit(1);
-	}
-	child_thread();
-	int thread2 = pthread_create(&parent, NULL, child_thread, NULL);
+	int thread2 = pthread_create(&children, NULL, child_thread, NULL);
 	if (thread2)
 	{
 		perror("Error in creating thread");
 		exit(1);
 	}
+	parent_thread();
+
 	pthread_exit(NULL);
 	return 0;
 }
--- Assignment2/Q2/test.c
+++ Assignment2/Q2/test.c
@@ -1,2 +1,33 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+#define __NR_task_info 440
+
+long task_info_syscall(int pid1, char* filenm)
+{
+    return syscall(__NR_task_info,pid1,filenm);
+}
+
+int main(int argc, char *argv[])
+{
+    long syscall_working;
+    char file[256];
+    int pid1;
+    printf("Enter the pid: \n");
+    scanf("%d",&pid1);
+    printf("Enter the File Name: \n");
+    scanf("%s",file );
+    syscall_working = task_info_syscall(pid1,file);
+
+    if(syscall_working <0)
+    {
+        perror("Error ");
+    }
+    else
+        printf("Success! Data written to file.\n");
+    return 0;
+}
