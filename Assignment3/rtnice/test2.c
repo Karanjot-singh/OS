8f49e6fa5651018d6c4298e2bbd76592bbc2afdb
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-11-17 14:11:05 +0530

    Assignment 3 files added back

---
+++ Assignment3/rtnice/test2.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
+#include <string.h>
+#include <errno.h>
+
+unsigned long long nCr(unsigned long long n, unsigned long long r)
+{
+	if(n>=r)
+	{
+		if(r==0 || r==n)
+		{
+			return 1;
+		}
+		else
+		{
+			return nCr(n-1,r-1) + nCr(n-1,r);
+		}
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int main()
+{
+	long int pid=fork();
+
+	if(pid!=0)
+	{
+		long ret = syscall(323,pid,300);
+		printf("%s\n",strerror(ret));
+	}
+
+	if(pid==0)
+	{
+		int pid2=fork();
+		if(pid2 != 0)
+		{
+			long ret2 = syscall(323,pid2,200);
+			printf("%s\n",strerror(ret2));
+		}
+		if(pid2==0)
+		{
+			unsigned long long ans = nCr(30,15);
+			printf("Finished calculating 30C15\n");
+			printf("Process with PID %ld finished\n",getpid());
+		}
+		else
+		{
+			unsigned long long ans = nCr(30,15);
+			printf("Finished calculating 30C15\n");
+			printf("Process with PID %ld finished\n",getpid());
+			wait(pid2);
+		}
+	}
+	else
+	{
+			unsigned long long ans = nCr(30,15);
+			printf("Finished calculating 30C15\n");
+			printf("Process with PID %ld finished\n",getpid());
+			wait(pid);
+	}
+	return 0;
+}
