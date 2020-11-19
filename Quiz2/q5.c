1ae6f1ab749ae9095bcb4a1ee1fdc2e14ead37cf
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-11-19 06:55:12 +0530

    working

--- Quiz2/q5.c
+++ Quiz2/q5.c
@@ -9,82 +9,70 @@ This program provides a possible solution for first readers writers problem usin
 I have used 10 readers and 5 producers to demonstrate the solution. You can always play with these values.
 */
 
-pthread_mutex_t mutex;
-sem_t semaphore1;
-int cnt, shmid;
-cnt = 1;
-key_t key;
+pthread_mutex_t mutex ;
+sem_t semaphore1 ;
+int reader_count = 0 ;
+int mem_id ;
+key_t key ;
 
-int reader_count = 0;
 
-void *writer(void *wno)
+void *writer(void *wno )
 {
-	sem_wait(&semaphore1);
-	// char *str = (char*) shmat(shmid,(void*)0,0);
-	// str="Testing";
-	// shmdt(str);
-	// printf("Writer %d modified cnt to %s\n", (*((int *)wno)), str);
-	int *ptr = (int *)shmat(shmid, (void *)0, 0);
-	*ptr = 5;
-	printf("Writer %d modified cnt to %d\n", (*((int *)wno)), *ptr);
-	shmdt(ptr);
-	sem_post(&semaphore1);
+	sem_wait(&semaphore1 ) ;
+	// str="Testing" ;
+	// shmdt(str ) ;
+	int *ptr = (int * )shmat(mem_id, (void * )0, 0 ) ;
+	*ptr = 5 ;
+	printf(" Writer%d- writes %d\n", (*( (int * )wno ) ), *ptr ) ;
+	shmdt(ptr ) ;
+	sem_post(&semaphore1 ) ;
 }
-void *reader(void *rno)
+void *reader(void *rno )
 {
 	// Reader acquire the lock before modifying reader_count
-	pthread_mutex_lock(&mutex);
-	reader_count++;
-	if (reader_count == 1)
+	pthread_mutex_lock(&mutex ) ;
+	reader_count++ ;
+	if (reader_count == 1 )
 	{
-		sem_wait(&semaphore1); // If this id the first reader, then it will block the writer
+		sem_wait(&semaphore1 ) ; // If this id the first reader, then it will block the writer
 	}
-	pthread_mutex_unlock(&mutex);
+	pthread_mutex_unlock(&mutex ) ;
 	// Reading Section
-	int *ptr = (int *)shmat(shmid, (void *)0, 0);
-	printf("Reader %d: read cnt as %d\n", *((int *)rno), *ptr);
-    shmdt(ptr); 
-    shmctl(shmid,IPC_RMID,NULL);
+	int *ptr = (int * )shmat(mem_id, ( void * )0, 0 ) ;
+	printf(" Reader%d- reads %d\n", *( ( int * )rno ), *ptr ) ;
+	shmdt(ptr ) ;
+	shmctl(mem_id, IPC_RMID, NULL ) ;
 
 	// Reader acquire the lock before modifying reader_count
-	pthread_mutex_lock(&mutex);
-	reader_count--;
-	if (reader_count == 0)
+	pthread_mutex_lock(&mutex ) ;
+	reader_count-- ;
+	if (reader_count == 0 )
 	{
-		sem_post(&semaphore1); // If this is the last reader, it will wake up the writer.
+		sem_post(&semaphore1 ) ; // If this is the last reader, it will wake up the writer.
 	}
-	pthread_mutex_unlock(&mutex);
+	pthread_mutex_unlock(&mutex ) ;
 }
 
-int main()
+int main( )
 {
-	key = ftok("Karanjot", 65);
-	shmid = shmget(key, 1024, 0666 | IPC_CREAT);
-	pthread_t read[10], write[5];
-	pthread_mutex_init(&mutex, NULL);
-	sem_init(&semaphore1, 0, 1);
+	key = ftok("Karanjot", 65 ) ;
+	mem_id = shmget(key, 1024, 0666 | IPC_CREAT ) ;
 
-	int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //Just used for numbering the producer and consumer
 
-	for (int i = 0; i < 5; i++)
-	{
-		pthread_create(&write[i], NULL, (void *)writer, (void *)&a[i]);
-	}
-	for (int i = 0; i < 10; i++)
-	{
-		pthread_create(&read[i], NULL, (void *)reader, (void *)&a[i]);
-	}
+	pthread_t read[10], write[5] ;
+	pthread_mutex_init(&mutex, NULL ) ;
+	sem_init(&semaphore1, 0, 1 ) ;
 
-	for (int i = 0; i < 5; i++)
-	{
-		pthread_join(write[i], NULL);
-	}
-	for (int i = 0; i < 10; i++)
-	{
-		pthread_join(read[i], NULL);
-	}
-	pthread_mutex_destroy(&mutex);
-	sem_destroy(&semaphore1);
+	int a = 1 ;
+	int b = 2 ;
+	pthread_create(&write[0], NULL, (void * )writer, (void * )&a ) ;
+	pthread_create(&read[0], NULL, (void * )reader, (void * )&a ) ;
+	pthread_create(&read[1], NULL, (void * )reader, (void * )&b ) ;
+	pthread_join(write[0], NULL ) ;
+	pthread_join(read[0], NULL ) ;
+	pthread_join(read[1], NULL ) ;
 
-	return 0;
+	pthread_mutex_destroy(&mutex ) ;
+	sem_destroy(&semaphore1 ) ;
+	return 0 ;
 }
