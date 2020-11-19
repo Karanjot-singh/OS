3671e18f590d01a2be17bee80558b2909076bf1f
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-11-19 06:31:35 +0530

    changed to int

--- Quiz2/q5.c
+++ Quiz2/q5.c
@@ -1,8 +1,8 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
-#include <sys/ipc.h> 
-#include <sys/shm.h> 
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 /*
 This program provides a possible solution for first readers writers problem using mutex and semaphore.
@@ -11,7 +11,7 @@ I have used 10 readers and 5 producers to demonstrate the solution. You can alwa
 
 pthread_mutex_t mutex;
 sem_t semaphore1;
-int cnt,shmid;
+int cnt, shmid;
 cnt = 1;
 key_t key;
 
@@ -20,10 +20,14 @@ int reader_count = 0;
 void *writer(void *wno)
 {
 	sem_wait(&semaphore1);
-	char *str = (char*) shmat(shmid,(void*)0,0);
-	str="Testing";
-    shmdt(str); 
-	printf("Writer %d modified cnt to %s\n", (*((int *)wno)), str);
+	// char *str = (char*) shmat(shmid,(void*)0,0);
+	// str="Testing";
+	// shmdt(str);
+	// printf("Writer %d modified cnt to %s\n", (*((int *)wno)), str);
+	int *ptr = (int *)shmat(shmid, (void *)0, 0);
+	*ptr = 5;
+	printf("Writer %d modified cnt to %d\n", (*((int *)wno)), *ptr);
+	shmdt(ptr);
 	sem_post(&semaphore1);
 }
 void *reader(void *rno)
@@ -37,21 +41,21 @@ void *reader(void *rno)
 	}
 	pthread_mutex_unlock(&mutex);
 	// Reading Section
-    key_t key = ftok("Karanjot",65); 
-  
-    // shmget returns an identifier in shmid 
-    int shmid = shmget(key,1024,0666|IPC_CREAT); 
-  
-    // shmat to attach to shared memory 
-    char *str = (char*) shmat(shmid,(void*)0,0); 
-  
-    printf("Data read from memory: %s\n",str); 
-      
-    //detach from shared memory  
-    shmdt(str); 
-    
-    // destroy the shared memory 
-    shmctl(shmid,IPC_RMID,NULL);
+	key_t key = ftok("Karanjot", 65);
+
+	// shmget returns an identifier in shmid
+	int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+
+	// shmat to attach to shared memory
+	char *str = (char *)shmat(shmid, (void *)0, 0);
+
+	printf("Data read from memory: %s\n", str);
+
+	//detach from shared memory
+	shmdt(str);
+
+	// destroy the shared memory
+	shmctl(shmid, IPC_RMID, NULL);
 	printf("Reader %d: read cnt as %d\n", *((int *)rno), cnt);
 
 	// Reader acquire the lock before modifying reader_count
@@ -74,16 +78,14 @@ int main()
 
 	int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //Just used for numbering the producer and consumer
 
-
 	for (int i = 0; i < 5; i++)
 	{
 		pthread_create(&write[i], NULL, (void *)writer, (void *)&a[i]);
 	}
-		for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		pthread_create(&read[i], NULL, (void *)reader, (void *)&a[i]);
 	}
-
 
 	for (int i = 0; i < 5; i++)
 	{
